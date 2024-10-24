d89f307a34736ba1ab15f2d0d26f57fa8e02f71a
adityaoberai <adityaoberai1@gmail.com>
2024-10-24 14:29:03 +0530

    Add placeholders

--- src/lib/components/EmailOtp.svelte
+++ src/lib/components/EmailOtp.svelte
@@ -32,7 +32,7 @@
 		<form class="u-flex-vertical u-gap-8" on:submit={createEmailOtp}>
 			<div>
 				<label for="email">Email</label>
-				<input type="email" id="email" name="email" autocomplete="off" required />
+				<input type="email" id="email" name="email" autocomplete="off" placeholder="john.doe@test.com" required />
 			</div>
 			<button class="button" type="submit">Send OTP on email</button>
 		</form>
@@ -42,7 +42,7 @@
 		<form class="u-flex-vertical u-gap-8" on:submit={verifyEmailOtp}>
 			<div>
 				<label for="otp">OTP</label>
-				<input type="text" id="otp" name="otp" autocomplete="off" required />
+				<input type="text" id="otp" name="otp" autocomplete="off" placeholder="123456" required />
 			</div>
 			<button class="button" type="submit">Submit OTP</button>
 		</form>
--- src/lib/components/EmailPassword.svelte
+++ src/lib/components/EmailPassword.svelte
@@ -33,15 +33,15 @@
 		<form class="u-flex-vertical u-gap-8" on:submit={registerEmailPassword}>
 			<div>
 				<label for="name">Name</label>
-				<input type="text" id="name" name="name" value="" autocomplete="off" required />
+				<input type="text" id="name" name="name" value="" autocomplete="off" placeholder="John Doe" required />
 			</div>
 			<div>
 				<label for="email">Email</label>
-				<input type="email" id="email" name="email" value="" autocomplete="off" required />
+				<input type="email" id="email" name="email" value="" autocomplete="off" placeholder="john.doe@test.com" required />
 			</div>
 			<div>
 				<label for="password">Password</label>
-				<input type="password" id="password" name="password" value="" autocomplete="off" required />
+				<input type="password" id="password" name="password" value="" autocomplete="off" placeholder="Password" required />
 			</div>
 			<button class="button" type="submit">Register</button>
 		</form>
@@ -52,11 +52,11 @@
 		<form class="u-flex-vertical u-gap-8" on:submit={loginEmailPassword}>
 			<div>
 				<label for="email">Email</label>
-				<input type="email" id="email" name="email" value="" autocomplete="off" required />
+				<input type="email" id="email" name="email" value="" autocomplete="off" placeholder="john.doe@test.com" required />
 			</div>
 			<div>
 				<label for="password">Password</label>
-				<input type="password" id="password" name="password" value="" autocomplete="off" required />
+				<input type="password" id="password" name="password" value="" autocomplete="off" placeholder="Password" required />
 			</div>
 			<button class="button" type="submit">Login</button>
 		</form>
